b893deed1d970f3c95f5b18cf35127decfadfc8a
Tobi <tobystic@gmail.com>
2022-08-20 18:16:12 +0000

    update yaml and database files

--- lib/connectdb.php
+++ lib/connectdb.php
@@ -11,14 +11,14 @@ function connectdb($database = "inject_demodb")
     $port = $db_props['port'];
     $username = $db_props['user'];
     $password = $db_props['pass'];
-    $database = substr($db_props['path'], 1); // remove the slash from '/path'
+    //$database = substr($db_props['path'], 1); // remove the slash from '/path'
   } else {
     //$host = isset($_ENV['SQL_INJECTION_DB_HOST']) ? $_ENV['SQL_INJECTION_DB_HOST'] : 'localhost';
     $port = isset($_ENV['SQL_INJECTION_DB_PORT']) ? $_ENV['SQL_INJECTION_DB_PORT'] : 3306;
     $username = isset($_ENV['SQL_INJECTION_DB_USERNAME']) ? $_ENV['SQL_INJECTION_DB_USERNAME'] : 'sql_injection';
     $password = isset($_ENV['SQL_INJECTION_DB_PASSWORD']) ? $_ENV['SQL_INJECTION_DB_PASSWORD'] : 'foobar';
   }
-  $db = mysqli_connect(null, $username, $password, $database, $port);
+  $db = mysqli_connect(null, $username, $password, $database, null,"/cloudsql/sampleappgcp:us-west2:coffeeshop");
   if (!$db) {
     echo ("Connection failed: " . mysqli_connect_error());
   }
